c2b9f3b320a4ab67f1cee7401a95d2362963519e
Unknown <bdistin@gmail.com>
2018-08-12 20:14:24 -0500

    add externals

--- src/index.js
+++ src/index.js
@@ -6,3 +6,36 @@ module.exports = {
 	Client: require('./lib/Client'),
 	[plugin]: require('./lib/Client')[plugin]
 };
+
+/**
+ * @external GuildMember
+ * @see {@link https://discord.js.org/#/docs/main/master/class/GuildMember}
+ */
+/**
+ * @external Settings
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/class/Settings}
+ */
+/**
+ * @external GatewayStorage
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/class/GatewayStorage}
+ */
+/**
+ * @external GatewayDriver
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/class/GatewayDriver}
+ */
+/**
+ * @external Schema
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/class/Schema}
+ */
+/**
+ * @external Collection
+ * @see {@link https://discord.js.org/#/docs/main/master/class/Collection}
+ */
+/**
+ * @external GatewayGetPathOptions
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/typedef/GatewayGetPathOptions}
+ */
+/**
+ * @external GatewayJSON
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/typedef/GatewayJSON}
+ */
