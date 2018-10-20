d2e3f79385f583d6c60642b0c0ef5977d3c1b573
Frangu Vlad <kingdgrizzle@gmail.com>
2018-10-20 19:27:20 +0300

    misc: Add mjs exports

---
+++ src/index.mjs
@@ -0,0 +1,14 @@
+import { Client as KlasaClient } from 'klasa';
+
+const pluginSymbol = KlasaClient.plugin;
+
+// KlasaMember
+export { default as KlasaMember } from './lib/extensions/KlasaMember';
+
+// MemberGateway
+export { default as MemberGateway } from './lib/settings/MemberGateway';
+
+// MemberGatewayClient
+import Client from './lib/Client';
+export { Client };
+export const { plugin } = Client;
