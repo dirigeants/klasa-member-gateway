4ca6439a02b4646756f0382829a739d27a2df7b6
Frangu Vlad <kingdgrizzle@gmail.com>
2018-10-20 19:28:54 +0300

    fix: Hopefully export it?

--- src/index.mjs
+++ src/index.mjs
@@ -11,4 +11,4 @@ export { default as MemberGateway } from './lib/settings/MemberGateway';
 // MemberGatewayClient
 import Client from './lib/Client';
 export { Client };
-export const { plugin } = Client;
+export const { [pluginSymbol] } = Client;
