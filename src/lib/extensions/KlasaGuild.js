3fcd1da55e7de6d7b95413e009aa1db6e0aae107
Unknown <bdistin@gmail.com>
2019-03-04 11:08:38 -0600

    fix already populated bug

--- src/lib/extensions/KlasaGuild.js
+++ src/lib/extensions/KlasaGuild.js
@@ -11,12 +11,17 @@ Structures.extend('guild', Guild => {
 		constructor(...args) {
 			super(...args);
 
+			// Members are already cached in the super call, so we need to repopulate with our extended store
+			const { members } = this;
+
 			/**
 			 * Storage for KlasaMembers
 			 * @since 0.0.1
 			 * @type {KlasaGuildMemberStore}
 			 */
-			this.members = new KlasaGuildMemberStore();
+			this.members = new KlasaGuildMemberStore(this);
+
+			for (const [id, member] of members) this.members.set(id, member);
 		}
 
 	}
